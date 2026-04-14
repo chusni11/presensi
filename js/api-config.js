18354f763b87f20097866fff06b80cc595f87de9
chusni11 <chusni1987@gmail.com>
2026-04-14 12:48:43 +0700

    Update api-config.js

--- js/api-config.js
+++ js/api-config.js
@@ -1,3 +1,3 @@
 // Konfigurasi API Google Apps Script
 // Ganti URL ini dengan URL Web App dari Google Apps Script Anda (Setiap deploy akan ada URL baru)
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz_YOUR_SCRIPT_ID_HERE/exec"; 
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw7NRE9FBfUuAdpCaNBBZNoLwiVfUf09cF-yMhsqM8Atvm4nWt92cwkle5lOK8d9Q5g/exec"; 
