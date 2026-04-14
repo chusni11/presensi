8c618011b9be378e689bd54b220af264bde81aa2
chusni11 <chusni1987@gmail.com>
2026-04-14 12:40:42 +0700

    Add files via upload

---
+++ api-config.js
@@ -0,0 +1,3 @@
+// Konfigurasi API Google Apps Script
+// Ganti URL ini dengan URL Web App dari Google Apps Script Anda (Setiap deploy akan ada URL baru)
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz_YOUR_SCRIPT_ID_HERE/exec"; 
