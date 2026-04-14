2dea580804e69626147d66dbb60f5451d301bf03
chusni11 <chusni1987@gmail.com>
2026-04-14 12:49:14 +0700

    Delete api-config.js

--- api-config.js
+++
@@ -1,3 +0,0 @@
-// Konfigurasi API Google Apps Script
-// Ganti URL ini dengan URL Web App dari Google Apps Script Anda (Setiap deploy akan ada URL baru)
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw7NRE9FBfUuAdpCaNBBZNoLwiVfUf09cF-yMhsqM8Atvm4nWt92cwkle5lOK8d9Q5g/exec"; 
